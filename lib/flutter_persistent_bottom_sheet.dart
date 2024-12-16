9cf909e3696886796b53b34622aaab9a61d3d83b
Kamil Szczęk <kamil@szczek.dev>
2024-12-16 15:45:34 +0100

    feat: implement a widget for measuring its child in layout phase
    This widget is required as a workaround to sync the size of the bottom
    sheet and a sheet's placeholder in a scaffold. Without the placeholder,
    the bottom sheet would cover up a part of the scaffold's body, which
    is not great.

--- lib/flutter_persistent_bottom_sheet.dart
+++ lib/flutter_persistent_bottom_sheet.dart
@@ -2,6 +2,8 @@
 /// docking, and more.
 library;
 
+export 'package:flutter_persistent_bottom_sheet/src/measure_height.dart'
+    show MeasureHeight;
 export 'package:flutter_persistent_bottom_sheet/src/persistent_bottom_sheet.dart'
     show PersistentBottomSheet;
 export 'package:flutter_persistent_bottom_sheet/src/reference.dart'
